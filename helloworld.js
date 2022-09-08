80ce85f4cb790c7ee73681630bd32b3212a15d7d
midnighter67 <101069922+midnighter67@users.noreply.github.com>
2022-09-08 11:44:07 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('a new message\n')
+   response.end('Los Angeles Rams are Super Bowl LVI Champs!\n')
 }).listen(3333)
 
 // Console will print the message
